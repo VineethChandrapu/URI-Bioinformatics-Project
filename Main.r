f8f67ba16665d89146d82573d123df8929ae8b1a
Francisco Xavier Vu Alves Pereira <falvespereira@uri.edu>
2025-03-11 16:29:09 -0400

    skeleton code

---
+++ Main.r
@@ -0,0 +1,18 @@
+#imports #linter
+
+CES <- function(){
+    #cancer effect size
+    print("help")
+}
+
+getPDB <- function(PDBcode){
+    filepath <- ""
+    print(filepath)
+    return (filepath)
+}
+
+dataParsing <- function(PDB,CES){
+    Cordinates <- c(1:24) #This vector should be as big the amount of entries we have
+    
+    return (Cordinates)
+}
