c933c063841daf04f9405f18ad4530ec30be5350
Chuck2444 <cakanegbu24@uri.edu>
2025-04-01 19:32:19 -0400

    Update Main.r

--- Main.r
+++ Main.r
@@ -14,15 +14,38 @@ getPDB <- function(PDBcode){
 
 dataParsing <- function(PDB,CES){
     Cordinates <- c(1:24) #This vector should be as big the amount of entries we have
-    
-    return (Cordinates)
+return (Cordinates)
 }
 
 ## uhhh cancereffecetsizer tutorial 
 options(timeout = 600)
 install.packages(c("remotes", "BiocManager"))
-remotes::install_github("Townsend-Lab-Yale/cancereffectsizeR", dependencies = TRUE, repos = BiocManager::repositories())
+remotes::install_github("Townsend-Lab-Yale/cancereffectsizeR",
+ dependencies = TRUE, repos = BiocManager::repositories())
+dir.create("CES_tutorial")
+setwd("CES_tutorial")
+#loading CES and data.table package
+library(cancereffectsizeR)
+library(data.table)
+remotes::install_github("nvelden/NGLVieweR")
+library(NGLVieweR)
 
+# downloading new package readr
+install.packages("readr")
+library(readr)
+#downloading variant data from cBioPortal 
+### UPDATE CHANGE FILE TO PULL A DOWNLOADED LOCAL FILE 
+TCGA_maf_file <- "TCGA-LIHC.maf.gz"
+if (!file.exists(TCGA_maf_file)) {
+get_TCGA_project_MAF(project = "LIHC", file = TCGA_maf_file)
+}
+tcga_clinical <- fread(system.file())
+## trying with CES functions
+get_ces_signature_set(refset = )
 
 ### pulling MAF file fro cBIOportal; NOTE: after path to, use your actual path to downloaded MAF file
+### complete later, to makke path generic
 maf_data <-load_maf("path/to/..")
+
+### pulling MAF file from local directory
+maf_data <- load_maf("TCGA-HCC.maf.gz")
