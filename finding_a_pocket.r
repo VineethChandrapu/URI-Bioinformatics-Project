ccc3933cc420cbeb0c243e3ba6a77ed863e58c3d
Liv C <conradyor@uri.edu>
2025-04-22 02:16:05 -0400

    Nested DF Update

--- finding_a_pocket.r
+++ finding_a_pocket.r
@@ -1,11 +1,14 @@
-install.packages("tidyverse")
+# ---------- Libraries ----------
+if(!require(dplyr)){install.packages("dplyr")}
+if(!require(dplyr)){install.packages("tidyverse")}
+if(!require(dplyr)){install.packages("purrr")}
 library(tidyverse)
 library(dplyr)
 library(purrr)
+library(stringr)
 
 # ---------- Read In and Organize Data to Be Used ----------
-# Make a Dataframe:
-# REPLACE WITH ACTUAL DATA (needs to be read into data frame)
+# Create a fake data frame for testing
 df <- data.frame(
   AA <- c("AA1", "AA1", "AA1", "AA2", "AA2", "AA3", "AA3", "AA3"),
   Atom <- c("a1", "a2", "a3", "a1", "a2", "a1", "a2", "a3"),
@@ -17,83 +20,69 @@ df <- data.frame(
 colnames(df) <- c("AminoAcid", "Atom", "Mutation?", "X", "Y", "Z")
 df
 
-# Dataframe of mutation points
-# mut_df <- df[df$Mut == TRUE,]
+# Dataframe of mutated points only:
+mut_df <- df[df$Mut == TRUE,]
 
 
 # ---------- Part 1: Construct a Distance Matrix of ALL Amino Acids ----------
 # Input: dataframe of all amino acids, atoms, and coordinates
 # Output: matrix with minimum pairwise distance between each amino acid 
-# Diagonal values should be 0
-
 
 # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 # Make Matrices for each Amino Acid: (DOESN'T WORK RN)
-for (AA in df$AminoAcid)
-  {
-    coords_df <- subset(df, AminoAcid == AA, select=c('X', 'Y', 'Z'))
-
-    coords_mat <- as.matrix(coords_df)
-    rownames(coords_mat) <- c(df$Atom) #REPLACE WITH ROW NAMES VECTOR
-    coords_mat
-    
-    dist_mat <- dist(coords_mat, method = "euclidean", diag = TRUE, upper = FALSE)
-    dist_mat
-}
+#for (AA in df$AminoAcid)
+#  {
+#    coords_df <- subset(df, AminoAcid == AA, select=c('X', 'Y', 'Z'))
+#
+#    coords_mat <- as.matrix(coords_df)
+#    rownames(coords_mat) <- c(df$Atom) #REPLACE WITH ROW NAMES VECTOR
+#    coords_mat
+#    
+#    dist_mat <- dist(coords_mat, method = "euclidean", diag = TRUE, upper = FALSE)
+#    dist_mat
+#}
 # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
+# Find distance between every 2 amino acids:
 
-# Find distance between 2 amino acids:
-  # Group the data:
-location_df <- df[,-3]
-grouped_by_AA <- df %>%
-  group_by(AminoAcid) %>%
-  nest()
-grouped_by_AA
+# Group the data {use: grouped_by_AA}
+  location_df <- df[,-3]
+  grouped_by_AA <- df %>%
+    group_by(AminoAcid) %>%
+    nest()
+  grouped_by_AA
 
+# Euclidean function {use: euclidean_distance[i, j] }
+    euclidean_distance <- function(point1, point2) {
+      sqrt(sum((point1 - point2)^2))
+    }
 
-  # Construct pairwise matrices for all amino acids:
-euclidean_distance <- function(point1, point2) {
-  sqrt(sum((point1 - point2)^2))
-}
-
-just_AA2 <- grouped_by_AA$data[[2]]
-print(just_AA2)
-
-for(i in 2:grouped_by_AA$data) {
-  distances <- matrix(NA, nrow = nrow(grouped_by_AA$data[[i-1]]), ncol = nrow(grouped_by_AA$data[[i]]))
-  distances[i, j] <- euclidean_distance(
-    c(grouped_by_AA$data[[i-1]]$X[i], grouped_by_AA$data[[i-1]]$Y[i], grouped_by_AA$data[[i-1]]$Z[i]),
-    c(grouped_by_AA$data[[i]]$X[j], grouped_by_AA$data[[i]]$Y[j], grouped_by_AA$data[[i]]$Z[j])
-  )
-  assign(i, data.frame(split(dat[[i]], rep(letters[1:5], each = 3))))
-  print(distances)
-}
-
-
-# Construct pairwise matrices for all amino acids:
-just_AA2 <- grouped_by_AA$data[[2]]
-print(just_AA2)
-
+# How to use just one dataframe:
 nrow(grouped_by_AA$data[[1]])
 
-for(i in grouped_by_AA$data) {
-  matrix(sapply(1:nrow(grouped_by_AA$data[[1]]), function(x) euclidean_distance(mat1[x,], mat2[x,])), ncol = 5, byrow = FALSE)
+# Outputting wrong
+df_lengths <- grouped_by_AA %>%
+  for(i in .)
+  {
+    nrow(grouped_by_AA[[i]]) # Get the number of rows (length of col1) for each dataframe
+  }
+max(df_lengths)
+
+
+# Looping through each dataframe to get distance matrices
+for(i in 2:nrow(grouped_by_AA$data[[i]]))
+  {
+  for (j in 2:nrow(grouped_by_AA$data[[i+1]]))
+    {
+    distances <- matrix(NA, nrow = nrow(grouped_by_AA$data[[i-1]]), ncol = ncol(grouped_by_AA$data[[i]]))
+    distances[i, j] <- euclidean_distance(
+      c(grouped_by_AA$data[[i-1]]$X[i], grouped_by_AA$data[[i-1]]$Y[i], grouped_by_AA$data[[i-1]]$Z[i]),
+      c(grouped_by_AA$data[[i]]$X[j], grouped_by_AA$data[[i]]$Y[j], grouped_by_AA$data[[i]]$Z[j])
+    )
+  #assign(i, data.frame(split(dat[[i]], rep(letters[1:5], each = 3))))
+  print(distances)
+  }
 }
 
 
-# --------- GROSS VVV ----------
 
-
-
-
-
-# ---------- Part 2:  ----------
-
-
-# ---------- Part 3: Significance Matrix -----------
-# Run upper triangle with statistical significance against "closeness" 
-
-
-# ---------- Part 4: Reconstructing a Dataframe ----------
-sig_df <- data.frame()
